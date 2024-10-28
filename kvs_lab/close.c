5b826317fec546d552682cad8dcabac7c13a0c70
parksiwoon <parksw003@gmail.com>
2024-10-28 22:05:21 +0900

    feat: close.c - close 함수 구현

--- kvs_lab/close.c
+++ kvs_lab/close.c
@@ -1,7 +1,20 @@
+//설명: close 함수는 KVS의 모든 메모리를 해제
 #include "kvs.h"
 
 int close(kvs_t* kvs)
 {
 	/* do program */
+	node_t *current = kvs->header->forward[0];
+    while (current != NULL) {
+        node_t *next = current->forward[0];
+        free(current->value);
+        free(current->forward);
+        free(current);
+        current = next;
+    }
+    free(kvs->header->forward);
+    free(kvs->header);
+    free(kvs);
+
 	return 0;
 }
