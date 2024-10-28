aee1d9717c353da269de604dd5e782b820c8a3a9
parksiwoon <parksw003@gmail.com>
2024-10-28 22:08:15 +0900

    feat: main.c - main 함수 구현

--- kvs_lab/main.c
+++ kvs_lab/main.c
@@ -1,13 +1,40 @@
+//설명: query.dat 파일을 읽고, put 또는 get 작업을 수행한 뒤 결과를 answer.dat에 저장
 #include "kvs.h"
 
 int main()
 {
 	kvs_t* kvs = open();
 
+	if (!kvs) return -1;
+
+    FILE *query = fopen("query.dat", "r");
+    FILE *answer = fopen("answer.dat", "w");
+    if (!query || !answer) {
+        printf("Failed to open file\n");
+        return -1;
+    }
+
+    char operation[10], key[100], value[100];
+    while (fscanf(query, "%[^,],%[^,],%s", operation, key, value) != EOF) {
+        if (strcmp(operation, "put") == 0) {
+            put(kvs, key, value);
+        } else if (strcmp(operation, "get") == 0) {
+            char *result = get(kvs, key);
+            if (result) {
+                fprintf(answer, "%s\n", result);
+                free(result);
+            } else {
+                fprintf(answer, "-1\n");
+            }
+        }
+    }
+
+	/** 
 	if(!kvs) {
 		printf("Failed to open kvs\n");
 		return -1;
 	}
+	*/
 
 	// workload execution  
 	
@@ -16,7 +43,8 @@ int main()
 	// 		if get, seach the key in kvs and return the value. 
 	//		Return -1 if the key is not found  
 
-
+	fclose(query);
+    fclose(answer);
 	close(kvs);
 	
 	return 0;
