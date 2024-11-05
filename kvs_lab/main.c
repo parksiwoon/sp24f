e66fe3988b6dd28f2e6c8ba427d8877b615c2503
parksiwoon <parksw003@gmail.com>
2024-11-05 10:41:09 +0900

    refactor: strcmp put에서 set으로 변경

--- kvs_lab/main.c
+++ kvs_lab/main.c
@@ -16,9 +16,9 @@ int main()
 
     char operation[10], key[100], value[100];
     while (fscanf(query, "%[^,],%[^,],%s", operation, key, value) != EOF) {
-        if (strcmp(operation, "put") == 0) {
-            put(kvs, key, value);
-        } else if (strcmp(operation, "get") == 0) {
+       if (strcmp(operation, "set") == 0) {
+    put(kvs, key, value);
+} else if (strcmp(operation, "get") == 0) {
             char *result = get(kvs, key);
             if (result) {
                 fprintf(answer, "%s\n", result);
